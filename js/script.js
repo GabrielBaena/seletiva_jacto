6bf757c87e024f2d230a30d02ff4612a8a865c08
GabrielBaena <95707100+GabrielBaena@users.noreply.github.com>
2022-10-02 22:28:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const input = document.querySelector('.input__search')
 const buttonPrev = document.querySelector('.btn-prev')
 const buttonNext = document.querySelector('.btn-next')
 
+//grupo com as RGBs das cores
 const colours = [
   '#A8A77A',
   '#EE8130',
@@ -31,6 +32,7 @@ const colours = [
   '#D685AD',
 ]
 
+//grupo com os tipos
 const types = [
   'normal',
   'fire',
@@ -66,23 +68,29 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
+  //animação para mudar de pokemon
   pokemonName.innerHTML = 'Loading...'
   pokemonNumber.innerHTML = ''
 
   const data = await fetchPokemon(pokemon)
-
+  //caso o pokemon exista
   if (data) {
     pokemonImage.style.display = 'block'
     pokemonName.innerHTML = data.name
     pokemonNumber.innerHTML = data.id
-    if (data.id > 807) {
+
+    //pokemon da geração 8
+    if (data.id > 807 && data.id < 899) {
       pokemonImage.src =
         data['sprites']['versions']['generation-viii']['icons']['front_default']
       pokemonShiny.src =
         'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
       pokemonImage.style.left = '58%'
       pokemonImage.style.bottom = '60%'
-    } else {
+    } 
+
+    //pokemon das gerações 1 a 7
+    else if (data.id < 808){
       pokemonImage.src =
         data['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon'][
           'front_default'
@@ -96,6 +104,17 @@ const renderPokemon = async (pokemon) => {
       pokemonShiny.style.left = '75%'
       pokemonShiny.style.bottom = '56%'
     }
+
+    //pokemon n tem imagem (os ultimos da geração 8)
+    else{
+      pokemonImage.src =
+        'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
+      pokemonShiny.src = './images/nurse_joy.png'
+      pokemonShiny.style.left = '58%'
+      pokemonShiny.style.bottom = '57%'
+    }
+    
+    //edita a cor do quadrado da tipagem
     for (var i = 0; i < 18; i++) {
       if (data['types']['0']['type']['name'] == types[i]) {
         pokemonTypeI.style.backgroundColor = colours[i]
@@ -103,21 +122,33 @@ const renderPokemon = async (pokemon) => {
       }
     }
     pokemonTypeI.innerHTML = data['types']['0']['type']['name']
+
+    //verifica se o poquemom tem 2 tipos
     if (data['types']['1']) {
+
+      //edita a cor do quadrado da tipagem
       pokemonTypeII.innerHTML = data['types']['1']['type']['name']
       for (var i = 0; i < 18; i++) {
         if (data['types']['1']['type']['name'] == types[i]) {
           pokemonTypeII.style.backgroundColor = colours[i]
         }
       }
-    } else {
+    }
+    
+    //alinha o quadrado da imagem caso só tenha 1 tipo
+    else {
       pokemonTypeII.innerHTML = ''
       pokemonTypeII.style.backgroundColor = '#0000'
       pokemonTypeI.style.left = '40%'
     }
     input.value = ''
     searchPokemon = data.id
-  } else {
+
+  
+  } 
+
+  //caso o pokemon não exista
+  else {
     pokemonImage.style.display = 'none'
     pokemonName.innerHTML = 'Not found'
     pokemonNumber.innerHTML = ''
@@ -131,22 +162,35 @@ const renderPokemon = async (pokemon) => {
   }
 }
 
+//para pesquisar o pokemon pelo nome ou numero
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   renderPokemon(input.value.toLowerCase())
   searchPokemon = pokemonName
 })
 
+//ir para o próximo pokemon
 buttonPrev.addEventListener('click', () => {
   if (searchPokemon > 1) {
     searchPokemon -= 1
     renderPokemon(searchPokemon)
   }
+  else{
+    searchPokemon = 905
+    renderPokemon(searchPokemon)
+  }
 })
 
+//ir para o pokemon anterior
 buttonNext.addEventListener('click', () => {
-  searchPokemon += 1
-  renderPokemon(searchPokemon)
+  if (searchPokemon < 905){
+    searchPokemon += 1
+    renderPokemon(searchPokemon)
+  }
+  else{
+    searchPokemon = 1
+    renderPokemon(searchPokemon)
+  }
 })
 
 renderPokemon(searchPokemon)
